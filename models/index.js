90a1fef7556f0a99e69cb817a634aa25d4179ca4
jessHwood <jhorntvedt@gmail.com>
2017-04-10 15:42:24 -0600

    intial commit

--- models/index.js
+++ models/index.js
@@ -4,4 +4,6 @@ mongoose.connect( process.env.MONGODB_URI ||
                   process.env.MONGOHQ_URL || 
                   "mongodb://localhost/personal-api");
 
+mongoose.connect( process.env.MONGODB_URI || "https://warm-castle-32625.herokuapp.com/" );
+
 // module.exports.Campsite = require("./campsite.js.example");
