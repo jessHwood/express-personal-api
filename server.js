15b2dabc775c553d772f0c5644018bb82ab7f440
jessHwood <jhorntvedt@gmail.com>
2017-04-10 21:26:44 -0600

    update method list

--- server.js
+++ server.js
@@ -38,23 +38,23 @@ app.get('/', function homepage(req, res) {
 app.get('/api', function api_index(req, res) {
   // TODO: Document all your api endpoints below
   res.json({
-    name: "true", // CHANGE ME ;)
+    name: "Jess Wood", 
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentation_url: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    base_url: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    documentation_url: "https://github.com/jessHwood/express-personal-api", 
+    base_url: "https://warm-castle-32625.herokuapp.com/", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {method: "GET", path: "/api/profile", description: "Data about me"}, 
+      {method: "GET", path: "/api/vacations", description: "Show some of my vacations"}, 
+      {method: "GET", path: "/api/vacations/:id", description: "Show a vacation by Id"}
     ]
   });
 });
 
 app.get('/api/profile', function api_index(req, res) {
-  // TODO: Document all your api endpoints below
+
   res.json({
     name: "Jess Wood", 
-    message: "Welcome to my personal api! Here's what you need to know!",
     github_link: "https://github.com/jessHwood", 
     base_url: "https://warm-castle-32625.herokuapp.com/", 
     current_city: "Sweet Ridge",
@@ -77,6 +77,13 @@ app.get('/api/vacations/:id', function (req, res) {
   db.Vacation.findOne({_id: req.params.id }, function(err, data) {
     res.json(data);
   });
+});
+
+app.post('/api/vacations', function(req, res){
+    console.log('vacations create', req.body);
+    var newVacation = req.body;
+    vacations.push(newVacation);
+    res.json(newVacation);
 });
 /**********
  * SERVER *
